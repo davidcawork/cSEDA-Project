7104668803ba599f1c8b1e5a2c429736c241e91a
David <davidcawork@gmail.com>
2021-02-10 11:16:24 +0100

    Update visualizacion.c

--- src/LCD/visualizacion.c
+++ src/LCD/visualizacion.c
@@ -25,17 +25,26 @@ void representar_const_LCD (void) //Representa todo lo constante (NOMBRES Y LINE
 	drawString(130, 10, "HUMEDAD", WHITE, BLACK, MEDIUM);
 	drawString(130, 30, "(porcentaje)", WHITE, BLACK, SMALL);
 	
-	drawString(10, 80, "VELOCIDAD VIENTO", WHITE, BLACK, MEDIUM);
-	drawString(10, 100, "(rpm)", WHITE, BLACK, SMALL);
+	drawString(10, 70, "VELOCIDAD VIENTO", WHITE, BLACK, MEDIUM);
+	drawString(10, 90, "(rpm)", WHITE, BLACK, SMALL);
+
+	drawString(10, 140, "TEMP EXT", WHITE, BLACK, MEDIUM);
+	drawString(10, 160, "(grados)", WHITE, BLACK, SMALL);
 	
-	drawString(10, 150, "Umbral temperatura:", WHITE, BLACK, SMALL);
-	drawString(10, 165, "Ventilador: OFF", WHITE, BLACK, SMALL);
-	drawString(10, 180, "Alarma: OFF", WHITE, BLACK, SMALL);
+	drawString(130, 140, "PRESION", WHITE, BLACK, MEDIUM);
+	drawString(130, 160, "(porcentaje)", WHITE, BLACK, SMALL);
+	
+	drawString(10, 250, "Umbral temperatura:", WHITE, BLACK, SMALL);
+	drawString(10, 265, "Ventilador: OFF", WHITE, BLACK, SMALL);
+	drawString(10, 280, "Alarma: OFF", WHITE, BLACK, SMALL);
 
 	drawHorizontalLine(0,0,235,WHITE);
 	drawHorizontalLine(0,45,235,WHITE);
 	drawHorizontalLine(0,75,235,WHITE);
-	drawHorizontalLine(0,115,235,WHITE);
+	drawHorizontalLine(0,120,235,WHITE);
+	drawHorizontalLine(0,150,235,WHITE);
+	drawHorizontalLine(0,195,235,WHITE);
+	drawHorizontalLine(0,225,235,WHITE);
 }
 
 void representar_temp_hum(int ventilador, int alarma) //Representa las variables -> ventilador ON = '1'; alarma ON = '1'
@@ -45,25 +54,32 @@ void representar_temp_hum(int ventilador, int alarma) //Representa las variables
 
 	sprintf(array, "%2.2f", humedad);
 	drawString(130, 55, array, WHITE, BLACK, SMALL);		//Muesta valor humedad
+	
+	sprintf(array, "%2.2f", temperatura2);
+	drawString(10, 190, array, WHITE, BLACK, SMALL);			//Muestra valor temperatura
+	
+	sprintf(array, "%2.2f", presion);
+	drawString(130, 190, array, WHITE, BLACK, SMALL);		//Muesta valor humedad
 
 	sprintf(array, "%2.0f", umbral_temperatura);					//Muestra umbral de temperatura
-	drawString(180, 150, array, WHITE, BLACK, SMALL);
+	drawString(180, 250, array, WHITE, BLACK, SMALL);
 
 	if (ventilador == 1) //ventilador ON = '1'
-		drawString(10, 165, "Ventilador:  ON", WHITE, BLACK, SMALL);
+		drawString(10, 265, "Ventilador:  ON", WHITE, BLACK, SMALL);
 	else								 //ventilador OFF = '0'
-		drawString(10, 165, "Ventilador: OFF", WHITE, BLACK, SMALL);
+		drawString(10, 265, "Ventilador: OFF", WHITE, BLACK, SMALL);
 		
 	if (alarma == 1)		 //alarma ON = '1'
-		drawString(10, 180, "Alarma:  ON", WHITE, BLACK, SMALL);
+		drawString(10, 280, "Alarma:  ON", WHITE, BLACK, SMALL);
 	else								 //alarma OFF = '0'
-		drawString(10, 180, "Alarma: OFF", WHITE, BLACK, SMALL);
+		drawString(10, 280, "Alarma: OFF", WHITE, BLACK, SMALL);
 }
 
 void representar_viento(void)
 {
-	drawString(10, 125, "borrando", BLACK, BLACK, SMALL);		//Muestra valor velocidad viento
+	drawString(10, 115, "borrando", BLACK, BLACK, SMALL);		//Muestra valor velocidad viento
 	
 	sprintf(array, "%3.2f", veloc_viento);
-	drawString(10, 125, array, WHITE, BLACK, SMALL);		//Muestra valor velocidad viento
+	drawString(10, 115, array, WHITE, BLACK, SMALL);		//Muestra valor velocidad viento
 }
+
